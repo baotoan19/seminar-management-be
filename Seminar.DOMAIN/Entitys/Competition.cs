45f6f486e7c8448551d4084057c156ef882c172f
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-10-22 02:41:40 +0700

    Update --> Entity #V9

--- Seminar.DOMAIN/Entitys/Competition.cs
+++ Seminar.DOMAIN/Entitys/Competition.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Seminar.CORE.Base;
+using Seminar.DOMAIN.Enum;
 namespace Seminar.DOMAIN.Entitys
 {
     [Table("Competitions")]
@@ -14,8 +15,21 @@ namespace Seminar.DOMAIN.Entitys
         [StringLength(255)]
         [ForeignKey("Organizers")]
         public int? OrganizerId { get; set; }
-        public string Description {get; set;}
-        public string Destination {get; set;}
+        public string Description { get; set; }
+        public string Destination { get; set; }
+        [NotMapped]
+        public CompetitionStatus Status
+        {
+            get
+            {
+                DateTime now = DateTime.Now;
+                if (now < DateStart)
+                    return CompetitionStatus.Upcoming;
+                if (now >= DateStart && now <= DateEnd)
+                    return CompetitionStatus.Active;
+                return CompetitionStatus.Completed;
+            }
+        }
         public virtual Organizer Organizer { get; set; }
         public virtual ICollection<ResearchTopic> ResearchTopics { get; set; } = new List<ResearchTopic>();
         public virtual ICollection<Review_Committee> Review_Committees { get; set; } = new List<Review_Committee>();
---
+++ Seminar.INFRASTRUCTURE/Migrations/20241021192750_UpdateMigration#V9.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seminar.INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMigrationV9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Competitions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Competitions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
