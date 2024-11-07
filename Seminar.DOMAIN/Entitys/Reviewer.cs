f537c8f776d4fe91f2fa898ef4b2da7b72d56045
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-11-08 04:28:06 +0700

    Update --> Entity #V12

--- Seminar.DOMAIN/Entitys/Reviewer.cs
+++ Seminar.DOMAIN/Entitys/Reviewer.cs
@@ -9,6 +9,7 @@ namespace Seminar.DOMAIN.Entitys
     {
         public string? Name { get; set; }
         public string? NumberPhone { get; set; }
+        public string Email { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string? Sex { get; set; }
         [StringLength(255)]
---
+++ Seminar.INFRASTRUCTURE/Migrations/20241107181218_UpdateMigration#V2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seminar.INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMigrationV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Reviewers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Reviewers");
+        }
+    }
+}
