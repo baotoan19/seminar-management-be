e9441930dbdee86c1efad605dff0ecc33dd11e7d
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-11-12 17:45:53 +0700

    Delete --> ReviewerForm

--- Seminar.API/Controllers/OrganizerController.cs
+++ Seminar.API/Controllers/OrganizerController.cs
@@ -92,40 +92,4 @@ public class OrganizerController : ControllerBase
             data: null));
     }
 
-    [HttpPost("review-form")]
-    public async Task<IActionResult> CreateReviewForm(CreateReviewFormDto createReviewFormDto)
-    {
-        await _reviewFormService.CreateReviewFormAsync(createReviewFormDto);
-        return Ok(new BaseResponse<CreateReviewFormDto>(
-            statusCode: StatusCodes.Status200OK,
-            code: ResponseCodeConstants.SUCCESS,
-            message: "Review Form created successfully",
-            data: null));
-    }
-
-    [HttpPatch("review-form/{id}")]
-    public async Task<IActionResult> UpdateReviewForm(int id, UpdateReviewFormDto updateReviewFormDto)
-    {
-        await _reviewFormService.UpdateReviewFormAsync(id, updateReviewFormDto);
-        return Ok(new BaseResponse<UpdateReviewFormDto>(
-            statusCode: StatusCodes.Status200OK,
-            code: ResponseCodeConstants.SUCCESS,
-            message: "Review Form updated successfully",
-            data: null));
-    }
-
-    [HttpDelete("review-form/{id}")]
-    public async Task<IActionResult> DeleteReviewForm(int id)
-    {
-        await _reviewFormService.DeleteReviewFormAsync(id);
-        return Ok(new BaseResponse<string>(
-            statusCode: StatusCodes.Status200OK,
-            code: ResponseCodeConstants.SUCCESS,
-            message: "Review Form deleted successfully",
-            data: null));
-    }
-
-
-
-
 }
