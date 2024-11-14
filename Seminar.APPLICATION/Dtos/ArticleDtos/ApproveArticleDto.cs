564193dda2537c6f61d4a989cf096cf59fe09f87
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-11-15 06:14:02 +0700

    Modify --> Dto

--- Seminar.APPLICATION/Dtos/ArticleDtos/ApproveArticleDto.cs
+++ Seminar.APPLICATION/Dtos/ArticleDtos/ApproveArticleDto.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Seminar.APPLICATION.Dtos.ArticleDtos;
 
 public class ApproveArticleDto
 {
-    public bool IsAcceptedForPublication { get; set; }
+    [Range(0, 2, ErrorMessage = "Accepted for publication status must be between 0 and 2")]
+    public int AcceptedForPublicationStatus { get; set; }
 }
--- Seminar.APPLICATION/Dtos/ResearchTopicDtos/UpdateIsReviewAcceptanceDto.cs
+++ Seminar.APPLICATION/Dtos/ResearchTopicDtos/UpdateIsReviewAcceptanceDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Seminar.APPLICATION.Dtos.ResearchTopicDtos;
 
 public class UpdateIsReviewAcceptanceDto
 {
     public int ResearchTopicId { get; set; }
-    public bool IsReviewAcceptance { get; set; }
+    [Range(0, 2, ErrorMessage = "Review acceptance status must be 0, 1, or 2")]
+    public int ReviewAcceptanceStatus { get; set; }
 }
