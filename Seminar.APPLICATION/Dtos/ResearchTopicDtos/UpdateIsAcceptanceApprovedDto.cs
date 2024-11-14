3265c9de75da97b6bfc9d62f46f9a91afb1457e7
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-11-15 05:55:46 +0700

    Delete --> Modify Map Data

--- Seminar.APPLICATION/Dtos/ResearchTopicDtos/UpdateIsAcceptanceApprovedDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Seminar.APPLICATION.Dtos.ResearchTopicDtos;
-
-public class UpdateIsAcceptanceApprovedDto
-{
-    public int ResearchTopicId { get; set; }
-    public bool IsAcceptanceApproved { get; set; }
-}
--- Seminar.APPLICATION/Models/ArticleAuthorVM.cs
+++
@@ -1,6 +0,0 @@
-namespace Seminar.APPLICATION.Models;
-
-public class ArticleAuthorVM : AuthorVM
-{
-    public required string RoleName { get; set; }
-}
