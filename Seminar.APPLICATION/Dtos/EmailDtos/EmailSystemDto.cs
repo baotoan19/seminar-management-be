51fb64299b1c35fef8433c86b6e9f4579621dfae
KaiiDoo <117659628+baotoan19@users.noreply.github.com>
2024-11-21 01:03:46 +0700

    Add --> EmailStystem Dto

---
+++ Seminar.APPLICATION/Dtos/EmailDtos/EmailSystemDto.cs
@@ -0,0 +1,11 @@
+namespace Seminar.APPLICATION.Dtos.EmailDtos;
+
+public class EmailSystemDto
+{
+    public string ReceiverEmail { get; set; }
+    public string ReceiverName { get; set; }
+    public string SenderEmail { get; set; }
+    public string SenderName { get; set; }
+    public string Subject { get; set; }
+    public string Content { get; set; }
+}
